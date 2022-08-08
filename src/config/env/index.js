242caae35c6f1d56ad6796546f31712af11156fc
jill bhatt <bhattjill@gmail.com>
2022-08-08 17:02:54 +0530

    Debug backend url

--- src/config/env/index.js
+++ src/config/env/index.js
@@ -1,11 +1,9 @@
 const NODE_ENV = process.env.REACT_APP_NODE_ENV;
 
-let BE_URL;
+let BE_URL = 'https://mern-tms-server.herokuapp.com';
 
 if (NODE_ENV === 'development') {
     BE_URL = 'http://localhost:5000';
-} else if (NODE_ENV === 'production') {
-    BE_URL = 'https://mern-tms-server.herokuapp.com';
 }
 
 export default BE_URL;
