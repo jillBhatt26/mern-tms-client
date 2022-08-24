6c49edc2c5523df7cae192cf8d8fa7192d7d23f3
jill bhatt <bhattjill@gmail.com>
2022-08-24 12:11:35 +0530

    Changed production values

--- src/config/env/index.js
+++ src/config/env/index.js
@@ -1,6 +1,6 @@
-const NODE_ENV = 'development';
+const NODE_ENV = 'production';
 
-let BE_URL = 'https://mern-tms-server.herokuapp.com';
+let BE_URL = 'http://159.65.145.66/api';
 
 if (NODE_ENV === 'development') {
     BE_URL = 'http://localhost:5000';
