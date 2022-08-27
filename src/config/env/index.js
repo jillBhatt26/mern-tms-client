031640b08211c7c17983f0740d1585a7582db3a1
jill bhatt <bhattjill@gmail.com>
2022-08-27 12:28:37 +0530

    Changed dev url in config

--- src/config/env/index.js
+++ src/config/env/index.js
@@ -3,7 +3,7 @@ const NODE_ENV = process.env.REACT_APP_NODE_ENV;
 let BE_URL = process.env.REACT_APP_BE_URL;
 
 if (NODE_ENV === 'development') {
-    BE_URL = 'http://localhost:5000';
+    BE_URL = 'http://localhost:5000/api';
 }
 
 export default BE_URL;
