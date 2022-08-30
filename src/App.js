ad63117798d576015a7e4570846bb2669ec799a1
jill bhatt <bhattjill@gmail.com>
2022-08-30 15:47:29 +0530

    Added ugly header

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ const App = () => {
         <>
             <CssBaseline />
             <Nav />
+            <h1>Hello World!</h1>
             <Container>
                 <Routes>
                     <Route path="/login" element={<Login />} />
