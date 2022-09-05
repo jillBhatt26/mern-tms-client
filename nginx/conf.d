c6d0424ff63f5312b47243e9ed167d9defbdbb99
jill bhatt <bhattjill@gmail.com>
2022-09-05 15:29:27 +0530

    Local nginx redo

--- nginx/conf.d
+++ nginx/conf.d
@@ -1,17 +1,15 @@
-server {
-    listen       80;
-    server_name  localhost;
+listen       80;
+server_name  localhost;
 
-    include /etc/nginx/mime.types;
+include /etc/nginx/mime.types;
 
-    location / {
-        root   /usr/share/nginx/html;
-        index  index.html index.htm;
-        try_files $uri /index.html;
-    }
+location / {
+    root   /usr/share/nginx/html;
+    index  index.html index.htm;
+    try_files $uri /index.html;
+}
 
-    error_page   500 502 503 504  /50x.html;
-    location = /50x.html {
-        root   /usr/share/nginx/html;
-    }
+error_page   500 502 503 504  /50x.html;
+location = /50x.html {
+    root   /usr/share/nginx/html;
 }
